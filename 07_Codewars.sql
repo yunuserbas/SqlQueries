807f89d7d06e038d9898f8628d6f1fe7ea7559ba
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2022-09-29 09:31:44 +0200

    Create 07_Codewars.sql

---
+++ 07_Codewars.sql
@@ -0,0 +1,23 @@
+
+
+
+-- 1 you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50 --
+
+
+SELECT * FROM people WHERE age > 50;
+
+
+-- 2 you need to create a simple DISTINCT statement, you want to find all the unique ages --
+
+
+SELECT DISTINCT ages FROM people;
+
+
+-- 3 write a query that returns all rows in the custid, custname, and custstate columns from the customers table --
+
+
+SELECT * FROM customers WHERE column = 'custid' and 'custname' and 'custstate';
+
+
+
+

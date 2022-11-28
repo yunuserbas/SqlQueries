a64b1f082e63b9b267f1a8f08f0c3a1d83626b05
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2022-11-28 19:24:20 +0100

    Update and rename StrataScratch.sql to 10_StrataScratch.sql

--- 10_StrataScratch.sql
+++ 10_StrataScratch.sql
@@ -28,5 +28,15 @@ WHERE c.first_name in ('Jill', 'Eva')
 ORDER BY c.id ASC
 
 
+--- Popularity of Hack
+-- Question : Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
+-- Based on the above, find the average popularity of the Hack per office location.
+-- Output the location along with the average popularity.
+
+
+select location, avg(popularity) from facebook_hack_survey
+join facebook_employees on facebook_hack_survey.employee_id=facebook_employees.id
+group by location;
+
 
 

bcaf18313cfb0a5d21cc3ec5b5ff6d6ae3950651
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2022-12-08 13:51:43 +0100

    Update 12_StrataScratch_subquery.sql

--- 12_StrataScratch_subquery.sql
+++ 12_StrataScratch_subquery.sql
@@ -2,13 +2,13 @@
 
 -- Users By Average Session Time
 
-Problem: Calculate each user's average session time. A session is defined as the time difference between a page_load and page_exit. 
-For simplicity, assume a user has only 1 session per day and if there are multiple of the same events on that day, consider only the latest page_load and 
-earliest page_exit. Output the user_id and their average session time.
+-- Problem: Calculate each user's average session time. A session is defined as the time difference between a page_load and page_exit. 
+-- For simplicity, assume a user has only 1 session per day and if there are multiple of the same events on that day, consider only the latest page_load and 
+-- earliest page_exit. Output the user_id and their average session time.
 
 
 
-Solution:
+-- Solution:
 
 
 SELECT

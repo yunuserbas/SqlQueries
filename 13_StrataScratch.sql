5953dbec17352a8f100b21db7430660e39549346
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-02 22:07:28 +0100

    Update 13_StrataScratch.sql

--- 13_StrataScratch.sql
+++ 13_StrataScratch.sql
@@ -1,12 +1,12 @@
 
 
---- Question : Number of violations ---
+--- Question : Number of violations
 
---- You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year. --- 
---- If an inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of violations by year in ascending order.----
+--- You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year.
+--- If an inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of violations by year in ascending order.
 
 
---- Solution ---
+--- Solution
 
 
 SELECT date_part('year', inspection_date) AS year, COUNT(violation_id) AS n_inspections

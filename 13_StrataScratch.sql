8cebb02db61f8905b3f975b900132ff2da561d8b
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-02 22:15:44 +0100

    Update 13_StrataScratch.sql

--- 13_StrataScratch.sql
+++ 13_StrataScratch.sql
@@ -17,4 +17,25 @@ ORDER BY year ASC;
 
 
 
+--- Question : Highest Cost Orders
+
+--- Find the customer with the highest daily total order cost between 2019-02-01 to 2019-05-01. If customer had more than one order on a certain day, sum the order costs on daily basis. 
+--- Output customer's first name, total cost of their items, and the date.
+--- For simplicity, you can assume that every first name in the dataset is unique.
+
+
+--- Solution
+
+
+SELECT c.first_name, SUM(o.total_order_cost) AS total_cost, DATE(o.order_date) AS order_date
+FROM customers c
+JOIN orders o ON c.id = o.cust_id
+WHERE o.order_date BETWEEN '2019-02-01' AND '2019-05-01'
+GROUP BY c.first_name, DATE(o.order_date)
+ORDER BY total_cost DESC
+LIMIT 1;
+
+
+
+
 

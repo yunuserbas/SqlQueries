96be48c406d35d27fdb688f0e0e7f26c30802a17
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-03 20:46:56 +0100

    Update 14_CodeWars.sql

--- 14_CodeWars.sql
+++ 14_CodeWars.sql
@@ -34,6 +34,24 @@ SELECT Greeting FROM greetings
 WHERE Greeting = 'hello world!';
 
 
+-- Question: You work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your store. 
+-- Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+
+-- books table schema
+
+-- name
+-- author
+-- copies_sold
+
+
+-- Solution
+
+
+SELECT name, author, count(copies_sold) as "count" FROM books
+GROUP BY name, author
+ORDER BY "count" DESC 
+LIMIT 5;
+
 
 
 

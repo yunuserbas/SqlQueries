d131df9cee39df548742bb0c43cf01402fc31cee
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-13 22:43:05 +0100

    Create 18_PowerFuunction.sql

---
+++ 18_PowerFuunction.sql
@@ -0,0 +1,26 @@
+
+-- QUESTION
+-- Given the following table 'decimals':
+
+-- decimals table schema
+-- id
+-- number1
+-- number2
+-- Return a table with a single column result which is the output of number1 raised to the power of number2.
+
+
+-- SOLUTION
+
+SELECT POWER(number1, number2) AS result
+FROM decimals;
+
+
+
+
+
+
+
+
+
+
+

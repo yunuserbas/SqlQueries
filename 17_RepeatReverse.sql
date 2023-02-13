d20e57b8a12254057de12106edd6d5fb70418e62
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-13 22:37:39 +0100

    Create 17_RepeatReverse.sql

---
+++ 17_RepeatReverse.sql
@@ -0,0 +1,26 @@
+
+
+-- QUESTION
+-- Using our monsters table with the following schema:
+-- monsters table schema
+
+-- id
+-- name
+-- legs
+-- arms
+-- characteristics
+-- return the following table:
+
+-- output schema
+
+-- name
+-- characteristics
+-- Where the name is the original string repeated three times (do not add any spaces), 
+-- and the characteristics are the original strings in reverse (e.g. 'abc, def, ghi' becomes 'ihg ,fed ,cba').
+
+
+
+-- SOLUTION
+
+SELECT name || name || name AS name, REVERSE(characteristics) AS characteristics
+FROM monsters;

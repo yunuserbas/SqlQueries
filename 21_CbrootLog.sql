51986055a9a4dbb3a78755dcc881702c1da7415a
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-14 21:39:29 +0100

    Create 21_CbrootLog.sql

---
+++ 21_CbrootLog.sql
@@ -0,0 +1,27 @@
+
+
+-- QUESTION
+
+-- Given the following table 'decimals':
+-- decimals table schema
+
+-- id
+-- number1
+-- number2
+-- Return a table with two columns (cuberoot, logarithm) where the values in cuberoot are the cube root of those provided in number1 
+-- and the values in logarithm are changed to the natural logarithm of those in number2.
+
+
+
+-- SOLUTION
+
+SELECT
+  cbrt(number1) AS cuberoot,
+  log(number2) AS logarithm
+FROM
+  decimals;
+
+
+
+
+
